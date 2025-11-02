564e0f79278fcc47cec6059d54800111553747fb
Hebert Soares de Oliveira <hebertsoaresdeoliveira@gmail.com>
2025-11-02 15:14:43 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  html.classList.toggle('Light')
+
+
+  const img = document.querySelector('#profile img')
+
+  if(html.classList.contains('Light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+  }
+  
+
+  //Pegar a tag img
+
+  //se tiver light mode, adicionar a imagem light.
+  //se tiver sem light mode , manter a imagem normal.
+}
